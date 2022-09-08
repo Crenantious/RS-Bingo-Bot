2080fb141b46d9539eba83778bd10b5ce2ec9954
Crenantious <66210196+Crenantious@users.noreply.github.com>
2022-09-08 10:54:00 +0100

    Add solution and bot connection
    Need to supply the token as a secret.

---
+++ RSBingoBot/Bot.cs
@@ -0,0 +1,53 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace BingoBotEmbed
+{
+    public class Bot
+    {
+        const string token = "";
+
+        readonly string[] prefixes = new string[] { "." };
+
+        public static DiscordClient Client { get; private set; } = null!;
+        public static InteractivityExtension Interactivity { get; private set; } = null!;
+        public static CommandsNextExtension Commands { get; private set; } = null!;
+
+        public async Task RunAsync()
+        {
+            Client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            Client.UseInteractivity(new InteractivityConfiguration { });
+
+            Commands = Client.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = prefixes,
+                EnableDms = false,
+                EnableDefaultHelp = true,
+                DmHelp = false
+            });
+
+            SlashCommandsExtension? slash = Client.UseSlashCommands();
+
+            Client.Ready += OnClientReady;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        Task OnClientReady(DiscordClient client, ReadyEventArgs args)
+        {
+            Console.WriteLine("Bot ready");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RSBingoBot/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace BingoBotEmbed;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Bot bot = new();
+        bot.RunAsync().GetAwaiter().GetResult();
+    }
+}
