54c4b5c3998437fa5de968058037a59f4cd80c56
Crenantious <66210196+Crenantious@users.noreply.github.com>
2024-03-19 09:43:24 +0000

    Add SubmitDropEvidenceDPTests.

---
+++ RSBingo-Framework-Tests/TileSelectValidators tests/Data parsers/SubmitDropEvidenceDPTests.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="SubmitDropEvidenceDPTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace RSBingo_Framework_Tests.DataParsers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSBingo_Framework.DataParsers;
+
+[TestClass]
+public class SubmitDropEvidenceDPTests : TSVDataParserTestBase
+{
+    private SubmitDropEvidenceDP parser = null!;
+
+    [TestInitialize]
+    public override void TestInitialize()
+    {
+        base.TestInitialize();
+
+        UserOne = MockDBSetup.Add_User(DataWorker, 0, Team);
+
+        parser = new();
+    }
+
+    [TestMethod]
+    public void GiveData_Parse_TheSameDataIsGottenBack()
+    {
+        parser.Parse(TileOne, UserOne);
+
+        Assert.AreEqual(TileOne, parser.Tile);
+        Assert.AreEqual(UserOne, parser.User);
+    }
+}
